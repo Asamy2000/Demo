f6b76b600b4297d6d2c19f6210c10319566df5a9
Asamy2000 <samy07002@gmail.com>
2024-04-04 10:59:49 +0200

    Init

---
+++ DemoApi/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Hello from AWS!";
+        }
+    }
+}
